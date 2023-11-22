447059ef3ceda96f004a38eb9883be231ac503fe
hussainzahidgiki <hussainzahidgiki@gmail.com>
2023-11-22 23:17:57 +0000

    WIP

--- src/main/java/uk/ac/cardiff/bankingapp/Main.java
+++ src/main/java/uk/ac/cardiff/bankingapp/Main.java
@@ -5,11 +5,10 @@ import uk.ac.cardiff.bankingapp.transactions.Transaction;
 import uk.ac.cardiff.bankingapp.transactions.TransactionType;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
 
-    static void getBankAccounts(List<BankAccount> bankAccounts) {
+    static void displayBankAccounts(List<BankAccount> bankAccounts) {
         String leftAlignFormat = "| %-3d | %-23s | %-9d |%n";
         System.out.format("+-----+-------------------------+-----------+%n");
         System.out.format("| Id  | Account Holder Name     | Balance($)|%n");
@@ -20,97 +19,148 @@ public class Main {
         System.out.format("+-----+-------------------------+-----------+%n");
     }
 
-    static void getTransactions(BankAccount filteredBankAccount) {
+    static void displayBankAccount(BankAccount bankAccount) {
+        String leftAlignFormat = "| %-3d | %-23s | %-9d |%n";
+        System.out.format("+-----+-------------------------+-----------+%n");
+        System.out.format("| Id  | Account Holder Name     | Balance($)|%n");
+        System.out.format("+-------------------------------+-----------+%n");
+        System.out.format(leftAlignFormat, bankAccount.getId(), bankAccount.getAccountHolderName(), bankAccount.getAccountBalance());
+        System.out.format("+-----+-------------------------+-----------+%n");
+    }
+
+    static void displayTransactions(BankAccount bankAccount) {
         String leftAlignFormat = "| %-5s | %-23s | %-8d |%n";
         System.out.format("+--------+------------------------------+----------+%n");
         System.out.format("| Type   | Transaction Date             | Amount($)|%n");
         System.out.format("+---------------------------------------+----------+%n");
-        for (int i = 0; i < filteredBankAccount.getTransactions().size(); i++) {
-            System.out.format(leftAlignFormat, filteredBankAccount.getTransactions().get(i).getTransactionType().toString(), filteredBankAccount.getTransactions().get(i).getTransactionDate(), filteredBankAccount.getTransactions().get(i).getTransactionAmount());
+        for (int i = 0; i < bankAccount.getTransactions().size(); i++) {
+            System.out.format(leftAlignFormat, bankAccount.getTransactions().get(i).getTransactionType().toString(), bankAccount.getTransactions().get(i).getTransactionDate(), bankAccount.getTransactions().get(i).getTransactionAmount());
         }
         System.out.format("+--------+------------------------------+----------+%n");
     }
 
     public static void main(String[] args) {
 
+        Boolean exit = false;
 
-        System.out.println("Welcome to Hussain's Bank");
-        System.out.println("Enter 1 to list all bank accounts");
-        System.out.println("Enter 2 to create a new bank account");
-        System.out.println("Enter 3 to make a transaction");
-        System.out.println("Enter 4 to view transactions");
-
-        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Please enter the option from the menu above");
-        String optionSelected = scanner.nextLine();
-
-
-        List<Transaction> initTransactions = new ArrayList(Arrays.asList(new Transaction(50000, new Date(), TransactionType.CREDIT), new Transaction(50000, new Date(), TransactionType.CREDIT)));
+        List<Transaction> initTransactions = new ArrayList(Arrays.asList(new Transaction(50000, new Date(), TransactionType.DEBIT), new Transaction(50000, new Date(), TransactionType.CREDIT)));
         List<BankAccount> bankAccounts = new ArrayList(Arrays.asList(new BankAccount(1, "Hussain", 3000, initTransactions), new BankAccount(2, "Hamza", 2000, initTransactions)));
 
+        while (!exit) {
+            System.out.println("***** BANKING APPLICATION MAIN MENU *****");
+            System.out.println("* Enter 1 to list all bank accounts     *");
+            System.out.println("* Enter 2 to view a bank account        *");
+            System.out.println("* Enter 3 to create a new bank account  *");
+            System.out.println("* Enter 4 to make a transaction         *");
+            System.out.println("* Enter 5 to view transactions          *");
+            System.out.println("* Enter 6 to remove a bank account      *");
+            System.out.println("* Enter 7 to exit the application       *");
+            System.out.println("*****************************************");
 
-        switch (optionSelected) {
-            case "1": {
-                getBankAccounts(bankAccounts);
-                break;
-            }
+            Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+            System.out.print("Input: ");
+            String optionSelected = scanner.nextLine();
 
-            case "2": {
-                System.out.println("Please enter your new bank account details");
-                System.out.print("Account holder name:");
-                String newAccountHolderName = scanner.nextLine();
-                System.out.print("Account Balance:");
-                Integer newAccountBalance = scanner.nextInt();
-                BankAccount newBankAccount = new BankAccount(3, newAccountHolderName, newAccountBalance);
-                bankAccounts.add(newBankAccount);
-                getBankAccounts(bankAccounts);
-                break;
-            }
 
-            case "3": {
-                System.out.println("Please select account you want to make a transaction with");
-                getBankAccounts(bankAccounts);
-                System.out.print("Account Id:");
-                Integer payerAccountId = scanner.nextInt();
-                System.out.println("Please select recipient account");
-                getBankAccounts(bankAccounts);
-                System.out.print("Recipient Account Id:");
-                Integer recipientAccountId = scanner.nextInt();
-                System.out.print("Please enter amount you want to send: ");
-                Integer transactionAmount = scanner.nextInt();
+            switch (optionSelected) {
 
-                BankAccount recipientAccount = bankAccounts.stream().filter(item -> item.getId() == recipientAccountId).findAny().orElse(null);
-                BankAccount payerAccount = bankAccounts.stream().filter(item -> item.getId() == payerAccountId).findAny().orElse(null);
-
-                if (payerAccount != null && recipientAccount != null) {
-                    List<Transaction> currentPayerTransactions = new ArrayList<>(payerAccount.getTransactions());
-                    currentPayerTransactions.add(new Transaction(transactionAmount, new Date(), TransactionType.CREDIT));
-                    payerAccount.setTransactions(currentPayerTransactions);
-                    payerAccount.setAccountBalance(payerAccount.getAccountBalance() - transactionAmount);
-
-                    List<Transaction> currentRecipientTransactions = new ArrayList<>(recipientAccount.getTransactions());
-                    currentRecipientTransactions.add(new Transaction(transactionAmount, new Date(), TransactionType.DEBIT));
-                    recipientAccount.setTransactions(currentRecipientTransactions);
-                    recipientAccount.setAccountBalance(recipientAccount.getAccountBalance() + transactionAmount);
-
-                    getTransactions(payerAccount);
+                //list all bank accounts
+                case "1": {
+                    displayBankAccounts(bankAccounts);
+                    break;
                 }
-                break;
-            }
 
-            case "4": {
-                System.out.println("Please select which account you want to see transactions for");
-                getBankAccounts(bankAccounts);
-                System.out.print("Account Id:");
-                Integer accountId = scanner.nextInt();
-                BankAccount filteredBankAccount = bankAccounts.stream().filter(item -> item.getId() == accountId).findAny().orElse(null);
-                getTransactions(filteredBankAccount);
-                break;
-            }
+                //view a bank account
+                case "2": {
+                    System.out.print("Account Holder Name:");
+                    String accountHolderName = scanner.nextLine();
+                    BankAccount bankAccount = bankAccounts.stream().filter(item -> item.getAccountHolderName().equals(accountHolderName)).findAny().orElse(null);
+                    if (bankAccount != null) {
+                        displayBankAccount(bankAccount);
+                    }
+                    else {
+                        System.out.println("No account found !");
+                    }
 
-            case "5": {
-                System.out.println("Exit from menu");
-                break;
+                    break;
+                }
+
+                //create a new bank account
+                case "3": {
+                    System.out.println("Please enter your new bank account details");
+                    System.out.print("Account Holder Name:");
+                    String newAccountHolderName = scanner.nextLine();
+                    System.out.print("Account Balance:");
+                    Integer newAccountBalance = scanner.nextInt();
+
+                    BankAccount newBankAccount = new BankAccount(bankAccounts.get(bankAccounts.size()-1).getId()+1, newAccountHolderName, newAccountBalance);
+                    bankAccounts.add(newBankAccount);
+                    System.out.print("Bank account created successfully!");
+                    break;
+                }
+
+                //make a transaction
+                case "4": {
+                    System.out.println("Please select account you want to make a transaction with");
+                    displayBankAccounts(bankAccounts);
+                    System.out.print("Account Id:");
+                    Integer payerAccountId = scanner.nextInt();
+                    System.out.println("Please select recipient account");
+                    displayBankAccounts(bankAccounts);
+                    System.out.print("Recipient Account Id:");
+                    Integer recipientAccountId = scanner.nextInt();
+                    System.out.print("Please enter amount you want to send: ");
+                    Integer transactionAmount = scanner.nextInt();
+
+                    BankAccount recipientAccount = bankAccounts.stream().filter(item -> item.getId().equals(recipientAccountId)).findAny().orElse(null);
+                    BankAccount payerAccount = bankAccounts.stream().filter(item -> item.getId().equals(payerAccountId)).findAny().orElse(null);
+
+                    if (payerAccount != null && recipientAccount != null) {
+                        List<Transaction> currentPayerTransactions = new ArrayList<>(payerAccount.getTransactions());
+                        currentPayerTransactions.add(new Transaction(transactionAmount, new Date(), TransactionType.CREDIT));
+                        payerAccount.setTransactions(currentPayerTransactions);
+                        payerAccount.setAccountBalance(payerAccount.getAccountBalance() - transactionAmount);
+
+                        List<Transaction> currentRecipientTransactions = new ArrayList<>(recipientAccount.getTransactions());
+                        currentRecipientTransactions.add(new Transaction(transactionAmount, new Date(), TransactionType.DEBIT));
+                        recipientAccount.setTransactions(currentRecipientTransactions);
+                        recipientAccount.setAccountBalance(recipientAccount.getAccountBalance() + transactionAmount);
+
+                        System.out.print("Transaction successful!");
+                    }
+                    break;
+                }
+
+                case "5": {
+                    System.out.println("Please select which account you want to see transactions for");
+                    displayBankAccounts(bankAccounts);
+                    System.out.print("Account Id:");
+                    Integer accountId = scanner.nextInt();
+                    BankAccount filteredBankAccount = bankAccounts.stream().filter(item -> item.getId().equals(accountId)).findAny().orElse(null);
+                    displayTransactions(filteredBankAccount);
+                    break;
+                }
+
+                case "6":{
+                    System.out.println("Please select which account you want to remove");
+                    displayBankAccounts(bankAccounts);
+                    System.out.print("Account Id:");
+                    Integer accountId = scanner.nextInt();
+                    bankAccounts.removeIf(item-> item.getId().equals(accountId));
+                    System.out.println("Account Removed Successfully!");
+                    break;
+
+                }
+
+                case "7": {
+                    System.out.println("Exiting from menu");
+                    exit = true;
+                    break;
+                }
+
+                default:
+                    System.out.println("Invalid Input, Please try again");
+
             }
 
         }
